96f603844ec0c3a35a6bcdbeff73706e8490f521
cokerjesutayo <joyscoker20@gmail.com>
2024-07-09 11:10:58 +0100

    added sort to user app

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -116,7 +116,7 @@ const getUsersAppliedJobs = async (req, res) => {
     const user = await USER.findById({ _id: userId }).populate(
       "jobsApplied.job"
     );
-    const appliedJobs = user.jobsApplied;
+    const appliedJobs = user.jobsApplied.sort((a,b)=>b.dateApplied-a.dateApplied);
     res.status(200).json({ success: true, numofJobs:appliedJobs.length, jobs: appliedJobs });
   } catch (error) {
     console.log(error);
